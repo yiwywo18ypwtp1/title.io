3a8ac03d20968e42252e3ad1b1335e274cb3f623
Alexander Ivanitskiy <negalapetrovna77@gmail.com>
2025-06-03 15:09:27 +0300

    PORT fix (3001 -> 5000)

--- src/contexts/Context.js
+++ src/contexts/Context.js
@@ -7,7 +7,7 @@ export const UserProvider = ({ children }) => {
    const [user, setUser] = useState(null);
 
    useEffect(() => {
-      axios.get('http://localhost:3001/api/users/me', { withCredentials: true })
+      axios.get('http://localhost:5000/api/users/me', { withCredentials: true })
          .then(res => setUser(res.data))
          .catch(() => setUser(null));
    }, []);
