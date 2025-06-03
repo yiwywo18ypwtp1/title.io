3955820d86872f5f54f57016bbc7d8a44c2745b9
Alexander Ivanitskiy <negalapetrovna77@gmail.com>
2025-06-03 15:58:36 +0300

    cors origins updated

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors({
    origin: [
       "http://localhost:3000",
       "http://127.0.0.1:3000",
-      "https://www.title-io.xyz/",
+      "https://www.title-io.xyz",
    ],
    credentials: true
 }));
