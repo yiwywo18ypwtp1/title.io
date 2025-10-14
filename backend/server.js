41f2e5b8e5ac8ec9b1e5e5cc1dda6d0ae6f21646
yiwywo18ypwtp1 <negalapetrovna77@gmail.com>
2025-10-14 17:15:25 +0200

    fix origins issue

--- backend/server.js
+++ backend/server.js
@@ -8,19 +8,28 @@ import { connectToDB } from "./db.js";
 
 const app = express();
 
+const allowedOrigins = [
+   "http://localhost:3000",
+   "http://127.0.0.1:3000",
+   "https://www.title-io.xyz",
+];
+
 app.use(cors({
-   origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "https://www.title-io.xyz",
-   ],
+   origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+         const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+         return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+   },
    credentials: true,
 }));
 
 app.use(express.json());
 app.use("/api/users", usersRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.get("/api/hello", (req, res) => {
    res.status(200).json("hello");
